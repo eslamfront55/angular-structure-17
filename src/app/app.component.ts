0a1d6ce94369e152b1575074ce1b9bbe090a71b8
eslamfront55 <eslamfront55@gmail.com>
2023-12-14 10:47:05 +0200

    fixed

---
+++ src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { Meta } from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'angular-17-ssr';
+
+  constructor(
+    private meta: Meta
+  ) { }
+  ngOnInit(): void {
+    this.meta.updateTag({ name: 'title', content: 'App started' })
+  }
+}
