bdcdf2fdb249982283bb0070b342ec14cabef466
eslamfront55 <eslamfront55@gmail.com>
2023-12-14 11:44:32 +0200

    prime ng

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,17 +2,18 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { Meta } from '@angular/platform-browser';
-
+import { RatingModule } from 'primeng/rating';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, RatingModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'angular-17-ssr';
-
+  value = 4;
   constructor(
     private meta: Meta
   ) { }
