87a6148d3448258fa3e1d3acde9d2889e084dadd
eslamfront55 <eslamfront55@gmail.com>
2023-12-18 15:41:53 +0200

    add place-details in meta

--- src/app/components/layout/footer/footer.component.ts
+++ src/app/components/layout/footer/footer.component.ts
@@ -1,101 +1,251 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import { CommonModule, NgOptimizedImage } from '@angular/common';
-import { patterns } from '../../../configs/patternValidation';
-import { FormBuilder, Validators } from '@angular/forms';
-import { TranslateModule } from '@ngx-translate/core';
-import { Router } from '@angular/router';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+
+import { ChangeDetectorRef, Component, ElementRef, Inject, OnInit, PLATFORM_ID, QueryList, ViewChild, ViewChildren } from '@angular/core';
+
+import { CommonModule, Location, isPlatformBrowser, isPlatformServer } from '@angular/common';
+
+import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+
+
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
+
+import { InputTextModule } from 'primeng/inputtext';
+import { Meta, Title } from '@angular/platform-browser';
+import { CarouselModule } from 'primeng/carousel';
+import { GalleriaModule } from 'primeng/galleria';
+import { TabViewModule } from 'primeng/tabview';
+import { SidebarModule } from 'primeng/sidebar';
+import { RatingModule } from 'primeng/rating';
 import { Subscription } from 'rxjs';
+import { MetadataService } from '../../../services/generic/SEO/metadata.service';
+import { keys } from '../../../configs/localstorage-key';
+
+declare var google: any;
+
 @Component({
-  standalone: true,
-  imports: [CommonModule, TranslateModule, NgOptimizedImage],
   selector: 'app-footer',
+  standalone: true,
+  imports: [
+    CommonModule, FormsModule, ReactiveFormsModule, RouterModule, GalleriaModule, TabViewModule, CarouselModule, SidebarModule, RatingModule,
+    InputTextModule, TranslateModule
+  ],
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
-
 export class FooterComponent implements OnInit {
   private unsubscribe: Subscription[] = [];
-  // imageBaseUrl: any = environment?.imageBaseUrl;
   currentLanguage: any;
+  imageBaseUrl: any = 'https://dashboard.hawdaj.net';
+  currentLoginInformation: any;
 
-  @Output() openPlaceHandler = new EventEmitter();
+  isLoadingSavePlace: boolean = false;
+  @ViewChild('locationTab') locationTab!: ElementRef;
+  activeIndex: any = 0;
+  // Start Map Configs
+  selecedMarker: any = null;
 
-  isLoadingBtn: boolean = false;
-  categoriesList: any = [];
+  zoom: any = 5;
+  markerPositions: any[] = [];
 
-  storesCategoriesList: any = [];
-  restaurantCategories: any = [];
 
-  emailForm = this.fb.group(
+  placeDetails: any;
+  isLoadingPlaceDetails: boolean = false;
+  placeId: any;
+
+  showGalleria: boolean = false;
+
+  relatedPlaces: any;
+  isLoadingRelatedPlaces: boolean = false;
+
+  form = this.fb.group(
     {
-      email: ['', [Validators.required, Validators.pattern(patterns?.email)]],
-    },
-    { updateOn: "blur" }
+      status: [null, [Validators.required]],
+      massage: ['', { validators: [Validators.required, Validators.minLength(10)], updateOn: 'blur' }],
+    }
   );
   get formControls(): any {
-    return this.emailForm?.controls;
+    return this.form?.controls;
   }
+  isLoadingBtn: boolean = false;
+
+  chat: any = [];
+  isLoadingChat: boolean = false;
+  selectedFile: any = '';
+  chatForm = this.fb.group({
+    message: ['', { validators: [Validators.required], updateOn: 'change' }],
+  });
+  fullUrl: any = null;
+  displayChat: boolean = false;
+  siteName!: String;
 
   constructor(
-    // private alertsService: AlertsService,
-    // private placesService: PlacesService,
-    // public publicService: PublicService,
-    // private homeService: HomeService,
+    @Inject(PLATFORM_ID) private platformId: Object,
+    private metadataService: MetadataService,
+    private activatedRoute: ActivatedRoute,
+    private cdr: ChangeDetectorRef,
+    private location: Location,
     private fb: FormBuilder,
-    private router: Router
+    private router: Router,
+    private translateService: TranslateService
   ) { }
 
   ngOnInit(): void {
-    // this.placesService.categoriesListSubj.subscribe((res: any) => {
-    //   this.categoriesList = this.publicService.slicedData(res, 4);
-    // });
-    // this.publicService.storeSubjCategory.subscribe((res: any) => {
-    //   this.storesCategoriesList = this.publicService.slicedData(res, 4);
-    // });
-    // this.publicService.restaurantSubjCategory.subscribe((res: any) => {
-    //   this.restaurantCategories = this.publicService.slicedData(res, 4);
-    // });
-  }
-
-  openPlaceWithCategoryId(id?: any): void {
-    this.openPlaceHandler?.emit();
-    this.router?.navigate(['/places', { category: id?.id }]);
-    // id ? this.publicService?.placeCategory?.next(id) : '';
-  }
-  openStoresWithCategoryId(storeCat?: any): void {
-    this.router?.navigate(['/stores/list', { category: storeCat?.id }]);
-    // this.publicService?.storeCategory?.next(storeCat)
-  }
-  openRestaurantWithCategoryId(category?: any): void {
-    this.router?.navigate(['/restaurants/list', { category: category?.id }]);
-    // this.publicService?.restaurantCategory?.next(category)
-  }
-
-  submit(): void {
-    if (this.emailForm?.valid) {
-      this.isLoadingBtn = true;
-      let data = {
-        email: this.emailForm?.value?.email
-      };
-      // this.homeService?.subscribe(data)?.subscribe(
-      //   (res: any) => {
-      //     if (res?.code == 200) {
-      //       this.emailForm.reset();
-      //       res?.message ? this.alertsService?.openToast('success', res?.message) : '';
-      //       this.isLoadingBtn = false;
-      //     } else {
-      //       res?.message ? this.alertsService?.openToast('error', res?.message) : '';
-      //       this.isLoadingBtn = false;
-      //     }
-      //   },
-      //   (err: any) => {
-      //     err ? this.alertsService?.openToast('error', err) : '';
-      //     this.isLoadingBtn = false;
-      //   }
-      // );
-    } else {
-      // this.publicService.validateAllFormFields(this.emailForm);
+    this.siteName = 'هودج';
+    if (isPlatformBrowser(this.platformId)) {
+      if (JSON.parse(window?.localStorage?.getItem(keys?.userLoginData) || '{}')?.user) {
+        this.currentLoginInformation = JSON.parse(window?.localStorage?.getItem(keys?.userLoginData) || '{}')?.user;
+      }
     }
+    this.initPageData();
+  }
+  private initPageData(): void {
+    this.setupBrowserSpecificTasks();
+
+    this.getPLaceDataById(this.placeId);
+
+  }
+  private setupBrowserSpecificTasks(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      this.currentLanguage = localStorage.getItem(keys.language);
+    }
+  }
+
+  getPLaceDataById(id: any, preventLoading?: boolean): void {
+    preventLoading ? '' : this.isLoadingPlaceDetails = true;
+    this.metadataService?.getPLaceById(id)?.subscribe(
+      (res: any) => {
+        if (res?.code == 200) {
+          this.placeDetails = res?.data;
+          this.cdr.detectChanges();
+          if (isPlatformBrowser(this.platformId)) {
+            this.updateMetaTags();
+            this.cdr.detectChanges();
+          }
+          if (isPlatformServer(this.platformId)) {
+            this.updateMetaTags();
+            this.cdr.detectChanges();
+          }
+          this.placeDetails['rate'] = Math.ceil(this.placeDetails?.rate);
+
+          if (this.placeDetails?.region?.name && this.placeDetails?.city?.name) {
+            this.placeDetails['address_name'] = this.placeDetails?.region?.name + ', ' + this.placeDetails?.city?.name;
+          } else if (this.placeDetails?.region?.name) {
+            this.placeDetails['address_name'] = this.placeDetails?.region?.name;
+          } else if (this.placeDetails?.city?.name) {
+            this.placeDetails['address_name'] = this.placeDetails?.city?.name;
+          }
+          this.markerPositions = [
+            {
+              lat: this.placeDetails?.lat,
+              lng: this.placeDetails?.long,
+              place_icon: 'assets/images/icons/location2.svg',
+              icon: {
+                url: this.placeDetails?.place_icon ? this.placeDetails?.place_icon : 'assets/images/icons/location2.svg',
+                size: this.placeDetails?.place_icon ? new google.maps.Size(30, 30) : new google.maps.Size(50, 50),
+              },
+              content: {
+                id: this.placeDetails?.id,
+                title: this.placeDetails.title,
+                location_name: this.placeDetails.address_name,
+                address: this.placeDetails.address,
+                rate: this.placeDetails.rate,
+                reviews: this.placeDetails?.review,
+                icon: this.placeDetails.place_icon,
+                thumbil_image: this.placeDetails.image ? this.imageBaseUrl + '/' + this.placeDetails?.image : 'assets/images/icons/location2.svg'
+              }
+            }
+          ];
+          this.cdr.detectChanges();
+          this.isLoadingPlaceDetails = false;
+        } else {
+
+        }
+        this.isLoadingPlaceDetails = false;
+      },
+      (err: any) => {
+        this.isLoadingPlaceDetails = false;
+      }
+    );
+  }
+  private updateMetaTags(): void {
+    this.metadataService.updateTitle(`${this.siteName} | ${this.placeDetails.title}`);
+    this.metadataService.updateMetaTagsName([
+      { name: 'title', content: `${this.siteName} | ${this.placeDetails.title}` },
+      { name: 'description', content: this.placeDetails.description },
+    ]);
+    this.metadataService.updateMetaTagsProperty([
+      { property: 'og:url', content: `/places/details/${this.placeDetails.slug}` },
+      { property: 'og:title', content: `${this.siteName} | ${this.placeDetails.title}` },
+      { property: 'og:description', content: this.placeDetails.description },
+      { property: 'og:image', content: this.imageBaseUrl + '/' + this.placeDetails.image },
+    ]);
+  }
+
+  savePlace(): void {
+
+  }
+  goToLocation(): void {
+    if (this.locationTab) {
+      this.activeIndex = 2;
+      if (isPlatformBrowser(this.platformId)) {
+        this.locationTab.nativeElement.scrollIntoView({ behavior: 'smooth' });
+      }
+    }
+  }
+
+  recallNewPlaceDetails(id: number): void {
+    this.router.navigate(['/places/details/', id]);
+    if (isPlatformBrowser(this.platformId)) {
+      window.scrollTo(0, 0);
+    }
+    this.getPLaceDataById(id);
+    this.getRelatedPlaces(id);
+  }
+  getRelatedPlaces(id?: any, preventLoading?: boolean): void {
+
+  }
+
+  sendFeedbackFromPlaces(): void {
+  }
+  cancel(): void {
+    this.form?.reset();
+  }
+
+  getChatGpt(): void {
+    this.isLoadingChat = true;
+  }
+  onFileSelected(event: any): void {
+    this.selectedFile = event.target.files[0] ?? null;
+    if (this.selectedFile.size <= 5000) {
+      this.chatForm.patchValue({
+        message: this.selectedFile.name
+      });
+      // this.chatForm.setValue()
+    }
+  }
+  addToChat(e: any, message: any): void {
+    this.chat.push({ text: message?.value, type: 'two' }),
+      message.value = '';
+    setTimeout(() => {
+      this.chat.push({ text: 'Thank You', type: 'one' })
+    }, 5000)
+    if (isPlatformBrowser(this.platformId)) {
+      window.scrollTo(e.yPosition)
+    }
+  }
+  share(link: any): void {
+  }
+  back(): void {
+    this.location?.back();
+  }
+  showMap(): void {
+
+  }
+  showReviews(): void {
+
+  }
+  openComingSoon(): void {
+
   }
 
   ngOnDestroy(): void {
---
+++ src/app/services/generic/SEO/metadata.service.ts
@@ -0,0 +1,119 @@
+import { Meta, Title } from '@angular/platform-browser';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MetadataService {
+  constructor(private titleService: Title,
+    private metaService: Meta,
+    private http: HttpClient) { }
+
+  updateTitle(title: string): void {
+    this.titleService.setTitle(title);
+  }
+
+  getTitle(): string {
+    return this.titleService.getTitle();
+  }
+
+  private createTagObject(tag: { name?: string, property?: string, content: string, scheme?: string }): any {
+    const newTag: any = tag.name ? { name: tag.name, content: tag.content } : { property: tag.property, content: tag.content };
+    if (tag.scheme) {
+      newTag['scheme'] = tag.scheme;
+    }
+    return newTag;
+  }
+
+  addMetaTagsName(metaTags: { name: string, content: string, scheme?: string }[]): void {
+    this.metaService.addTags(metaTags.map(tag => this.createTagObject(tag)));
+  }
+
+  addMetaTagsProperty(metaTags: { property: string, content: string, scheme?: string }[]): void {
+    this.metaService.addTags(metaTags.map(tag => this.createTagObject(tag)));
+  }
+
+  updateMetaTagsName(metaTags: { name: string, content: string, scheme?: string }[]): void {
+    metaTags.forEach(tag => this.metaService.updateTag(this.createTagObject(tag)));
+  }
+
+  updateMetaTagsProperty(metaTags: { property: string, content: string, scheme?: string }[]): void {
+    metaTags.forEach(tag => this.metaService.updateTag(this.createTagObject(tag)));
+  }
+
+  removeMetaTagByName(name: string): void {
+    this.metaService.removeTag(`name='${name}'`);
+  }
+
+  removeMetaTagByProperty(property: string): void {
+    this.metaService.removeTag(`property='${property}'`);
+  }
+
+  getMetaTagsName(): any {
+    return this.metaService.getTags('name');
+  }
+  getMetaTagsProperty(): any {
+    return this.metaService.getTags('property');
+  }
+
+  setDefaultMetaTags(): any {
+    this.updateTitle('هودج');
+    this.addMetaTagsName([
+      { name: 'title', content: 'هودج' },
+      { name: 'description', content: "هل تعرف ما يمكنك توقعه في مناطق السعودية الـ13؟ تعرف على سر تميز كل منطقة، والمعالم التي لا بد من زيارتها في كل منها من خلال موقع هودج" },
+      { name: 'date', content: '2023-10-29T09:28:59+00:00' },
+
+      { name: 'twitter:card', content: 'summary_large_image' },
+      { name: 'twitter:url', content: 'https://metatags.io/' },
+
+      { name: 'twitter:title', content: 'هودج' },
+      { name: 'twitter:description', content: "هل تعرف ما يمكنك توقعه في مناطق السعودية الـ13؟ تعرف على سر تميز كل منطقة، والمعالم التي لا بد من زيارتها في كل منها من خلال موقع هودج" },
+      { name: 'twitter:image', content: 'https://metatags.io/images/meta-tags.png' },
+    ]);
+    this.addMetaTagsProperty([
+
+
+      { property: 'article:modified_time', content: '2023-10-29T09:28:59+00:00' },
+
+      { property: 'og:type', content: 'website' },
+      { property: 'og:url', content: 'https://metatags.io/' },
+      { property: 'og:title', content: 'هودج' },
+      { property: 'og:description', content: "هل تعرف ما يمكنك توقعه في مناطق السعودية الـ13؟ تعرف على سر تميز كل منطقة، والمعالم التي لا بد من زيارتها في كل منها من خلال موقع هودج" },
+      { property: 'og:image', content: 'https://metatags.io/images/meta-tags.png' },
+      { property: 'twitter:site_name', content: 'موقع هودج' }
+    ]);
+  }
+  updateDefaultMetaTags(): any {
+    this.updateTitle('هودج');
+    this.updateMetaTagsName([
+      { name: 'description', content: "هل تعرف ما يمكنك توقعه في مناطق السعودية الـ13؟ تعرف على سر تميز كل منطقة، والمعالم التي لا بد من زيارتها في كل منها من خلال موقع هودج" },
+      { name: 'title', content: 'هودج' },
+      { name: 'date', content: '2023-10-29T09:28:59+00:00' },
+
+      { name: 'twitter:card', content: 'summary_large_image' },
+      { name: 'twitter:url', content: 'https://metatags.io/' },
+
+      { name: 'twitter:title', content: 'هودج' },
+      { name: 'twitter:description', content: "هل تعرف ما يمكنك توقعه في مناطق السعودية الـ13؟ تعرف على سر تميز كل منطقة، والمعالم التي لا بد من زيارتها في كل منها من خلال موقع هودج" },
+      { name: 'twitter:image', content: 'https://metatags.io/images/meta-tags.png' },
+    ]);
+    this.updateMetaTagsProperty([
+
+
+      { property: 'article:modified_time', content: '2023-10-29T09:28:59+00:00' },
+
+      { property: 'og:type', content: 'website' },
+      { property: 'og:url', content: 'https://metatags.io/' },
+      { property: 'og:title', content: 'هودج' },
+      { property: 'og:description', content: "هل تعرف ما يمكنك توقعه في مناطق السعودية الـ13؟ تعرف على سر تميز كل منطقة، والمعالم التي لا بد من زيارتها في كل منها من خلال موقع هودج" },
+      { property: 'og:image', content: 'https://metatags.io/images/meta-tags.png' },
+      { property: 'twitter:site_name', content: 'موقع هودج' }
+    ]);
+  }
+
+  getPLaceById(id: any): Observable<any> {
+    return this.http.get(`https://dashboard.hawdaj.net/api/places/al-faw-village`);
+  }
+}
